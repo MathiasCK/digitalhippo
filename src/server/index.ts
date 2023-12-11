45efe20dc3afd820dec9e35978ebfd4e4fe9dfd3
mathiasck <mathias.ck2000@gmail.com>
2023-12-11 12:21:44 +0100

    Configure express server to selfhost nextjs

---
+++ src/server/index.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "../next-utils";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const main = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async cms => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use((req: Request, res: Response) => {
+    nextHandler(req, res);
+  });
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("NextJS ready");
+  });
+
+  app.listen(PORT, async () => {
+    payload.logger.info(
+      `Next.js App URL: ${process.env.NEXT_PUBLIC_PAYLOAD_SERVER_URL}`,
+    );
+  });
+};
+
+main();
