c56119203c070ed3e3740e02c326590226a56e98
mathiasck <mathias.ck2000@gmail.com>
2023-12-12 08:18:51 +0100

    Setup trpc

---
+++ src/lib/validators/account-credentials.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const AuthCredentialsValidator = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+  passwordConfirmation: z.string().min(8, { message: "Passwords must match" }),
+});
+
+export type AuthCredentials = z.infer<typeof AuthCredentialsValidator>;
