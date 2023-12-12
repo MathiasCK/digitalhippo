7538701d4347c2a0faea6c6e0c0ea33b7b72d0d2
mathiasck <mathias.ck2000@gmail.com>
2023-12-12 09:24:25 +0100

    Create users collection & generate payload types

---
+++ src/collections/Users.ts
@@ -0,0 +1,33 @@
+import { CollectionConfig } from "payload/types";
+
+const Users: CollectionConfig = {
+  slug: "users",
+  auth: true,
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      required: true,
+      defaultValue: "user",
+      admin: {
+        condition: (_, siblingData) => siblingData.role === "admin",
+      },
+      type: "select",
+      options: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "User",
+          value: "user",
+        },
+      ],
+    },
+  ],
+};
+
+export default Users;
