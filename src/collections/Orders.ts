9e3ccb22fdabce40f9aa203a479c366706efdef4
mathiasck <mathias.ck2000@gmail.com>
2024-01-17 11:54:55 +0100

    Create Orders collection

---
+++ src/collections/Orders.ts
@@ -0,0 +1,59 @@
+import { Access, CollectionConfig } from "payload/types";
+
+const yourOwnOrders: Access = ({ req: { user } }) => {
+  if (!user) return false;
+  if (user.role === "admin") return true;
+
+  return {
+    user: {
+      equals: user.id,
+    },
+  };
+};
+
+const Orders: CollectionConfig = {
+  slug: "orders",
+  admin: {
+    useAsTitle: "Your orders",
+    description: "Orders placed by you",
+  },
+  access: {
+    read: ({ req }) => yourOwnOrders({ req }),
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
+    create: ({ req }) => req.user.role === "admin",
+  },
+  fields: [
+    {
+      name: "_isPaid",
+      type: "checkbox",
+      access: {
+        read: ({ req }) => req.user.role === "admin",
+        create: () => false,
+        update: () => false,
+      },
+      admin: {
+        hidden: true,
+      },
+      required: true,
+    },
+    {
+      name: "user",
+      type: "relationship",
+      hidden: true,
+      admin: {
+        hidden: true,
+      },
+      relationTo: "users",
+    },
+    {
+      name: "products",
+      type: "relationship",
+      relationTo: "products",
+      required: true,
+      hasMany: true,
+    },
+  ],
+};
+
+export default Orders;
--- src/collections/index.ts
+++ src/collections/index.ts
@@ -2,3 +2,4 @@ export { default as Users } from "./Users";
 export { default as Products } from "./Products";
 export { default as ProductFiles } from "./ProductFiles";
 export { default as Media } from "./Media";
+export { default as Orders } from "./Orders";
