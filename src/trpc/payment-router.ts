a9148a8ebf598c104e15b59faff2cca63e180a14
mathiasck <mathias.ck2000@gmail.com>
2024-01-19 12:43:05 +0100

    Create payment router

---
+++ src/trpc/payment-router.ts
@@ -0,0 +1,88 @@
+import { z } from "zod";
+import { privateProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { getPayloadClient } from "../server/get-payload";
+import { stripe } from "../lib/stripe";
+import type Stripe from "stripe";
+
+export const paymentRouter = router({
+  createSession: privateProcedure
+    .input(
+      z.object({
+        productIds: z.array(z.string()),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx;
+      let { productIds } = input;
+
+      if (productIds.length === 0) {
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
+
+      const payload = await getPayloadClient();
+
+      const { docs: products } = await payload.find({
+        collection: "products",
+        where: {
+          id: {
+            in: productIds,
+          },
+        },
+      });
+
+      const filteredProducts = products.filter(product =>
+        Boolean(product.priceId),
+      );
+
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          products: filteredProducts.map(p => p.id),
+          user: user.id,
+        },
+      });
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+      for (const product of filteredProducts) {
+        line_items.push({
+          price: product.priceId!,
+          quantity: 1,
+          adjustable_quantity: {
+            enabled: false,
+          },
+        });
+      }
+
+      line_items.push({
+        price: process.env.STRIPE_TRANSACTION_FEE as string,
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false,
+        },
+      });
+
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card", "paypal"],
+          mode: "payment",
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items,
+        });
+
+        return { url: stripeSession.url };
+      } catch (error) {
+        console.log(error);
+        return {
+          url: null,
+        };
+      }
+    }),
+});
