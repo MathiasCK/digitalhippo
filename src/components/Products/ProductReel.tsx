7b8abdde50546f7bd4d2a6cabfeb31c1614972db
mathiasck <mathias.ck2000@gmail.com>
2024-01-19 09:57:22 +0100

    Fix minor map warning

--- src/components/Products/ProductReel.tsx
+++ src/components/Products/ProductReel.tsx
@@ -64,7 +64,11 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
             {productsMap.map((product, index) => (
-              <ProductListing key={index} index={index} product={product} />
+              <ProductListing
+                key={`product-${index}`}
+                index={index}
+                product={product}
+              />
             ))}
           </div>
         </div>
